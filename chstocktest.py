2a007d8072b958bb6c7ddd0c4ab1533fcb8714ef
iecjeff <iec.jeff@gmail.com>
2015-08-07 16:10:52 +0800

    Create

---
+++ chstocktest.py
@@ -0,0 +1,2 @@
+import chinastock as cs
+print(cs.get_stock_today(code='000001', exchange='SZ'))
---
+++ hello.py
@@ -0,0 +1,10 @@
+out=open("data.out","w")
+print("test", file=out)
+out.close()
+
+import pickle
+with open("mydata.pickle","wb") as mysavedata:
+    pickle.dump([1,2,"three"], mysavedata)
+with open("mydata.pickle","rb") as myrestoredata:
+    a_list=pickle.load(myrestoredata)
+print(a_list)
